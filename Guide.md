083d0425d739a6069973b74e7629a270933490e9
DHedgecock <hedgecock.d@gmail.com>
2017-09-09 18:59:27 -0700

    Theme is building with first draft colors

---
+++ Guide.md
@@ -0,0 +1,18 @@
+# Tranquil Theme Guide
+This theme uses a build system to manage setting the color variables for individual
+token and editor settings. See `build.js` for details.
+
+## Token Styles
+Currently the only token styles available are foreground and font-style. The
+following values are valid for font-style:
+- italic
+- bold
+- underline
+- italic bold
+- italic underline
+- bold underline
+- italic bold underline
+
+
+## Color Safe
+#3ec481
--- build.js
+++ build.js
@@ -4,41 +4,42 @@ const yaml = require('js-yaml')
 const load = path => yaml.safeLoad(readFileSync(path, 'utf-8'))
 
 // Load the base theme definition
-const theme = load('themes/base.yaml')
+let theme = load('theme/base.yaml')
 
 // Add Editor theming rules
 Object.assign(
   theme.colors,
-  load('themes/workbench.yaml'),
-  load('themes/terminal.yaml')
+  load('theme/workbench.yaml'),
+  load('theme/terminal.yaml')
 )
 
 // Add language highlighting rules
 theme.tokenColors = [
   ...theme.tokenColors,
-  ...load('themes/template.yaml'),
-  ...load('themes/markdown.yaml'),
-  ...load('themes/html.yaml'),
-  ...load('themes/css.yaml'),
-  ...load('themes/regex.yaml'),
-  ...load('themes/jsdoc.yaml')
+  ...load('theme/css.yaml'),
+  ...load('theme/docker.yaml'),
+  ...load('theme/javascript.yaml'),
+  ...load('theme/jsdoc.yaml'),
+  ...load('theme/markdown.yaml'),
+  ...load('theme/regex.yaml'),
+  ...load('theme/template.yaml')
 ]
 
 // Color Variables
-const colorVars = yaml.safeLoad(readFileSync('themes/color-vars.yaml', 'utf-8'))
+const colorVars = yaml.safeLoad(readFileSync('theme/color-vars.yaml', 'utf-8'))
 
 // Stringify all of the combined theme styles so we can run string regexes on it to
 // replace color variables with color values
-thme = JSON.stringify(theme, null, 2)
+theme = JSON.stringify(theme, null, 2)
 
 Object.keys(colorVars).forEach(color => {
   // Include a " prevents blue from matching in light-blue or blue-sky
-  theme = theme.replace(new RegExp(`"${color}"`, 'g'), `"${colorVars[color]}"`)
+  theme = theme.replace(new RegExp(`"\\${color}"`, 'g'), `"${colorVars[color]}"`)
 })
 
 // Base file has been extended with additional theme styles and color variables have
 // been replaced with Panda theme values. Write to /dist for consumption.
-writeFile('dist/Tranquil.json', base, err => {
+writeFile('dist/Tranquil.json', theme, err => {
   if (err) console.warn(err)
   console.log('Build finished')
 })
